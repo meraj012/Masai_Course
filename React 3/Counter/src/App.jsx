4388e5baa2d8f6988e9f7869dad53016b5550480
meraj012 <meraj84091@gmail.com>
2023-10-20 18:57:45 +0530

    Assignment

---
+++ React 3/Counter/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = ()=>{
+    setCount(count+1)
+    }
+
+  return (
+    <>
+      <h1>Count is {count}</h1>
+      <button onClick={handleClick}>Click</button>
+        
+    </>
+  )
+}
+
+export default App
